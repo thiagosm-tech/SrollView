16247a4bcc96de27af16d8c0a6356985e5fd31e8
Thiago <thiagosm.tech@gmail.com>
2021-08-07 13:25:55 -0300

    primeira versão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScrollViewComponent } from './scroll-view/scroll-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ScrollViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/scroll-view/scroll-view.component.ts
@@ -0,0 +1,63 @@
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-view',
+  templateUrl: './scroll-view.component.html',
+  styleUrls: ['./scroll-view.component.scss']
+})
+export class ScrollViewComponent implements OnInit {
+
+  @ViewChild('container') private container!: ElementRef;
+
+
+
+  @ViewChild('sectionOne') sectionOne!: ElementRef;
+  @ViewChild('sectionTwo') sectionTwo!: ElementRef;
+  @ViewChild('sectionThree') sectionThree!: ElementRef;
+  @ViewChild('sectionFour') sectionFour!: ElementRef;
+  @ViewChild('sectionFive') sectionFive!: ElementRef;
+
+  oneOffset!: number;
+  TwoOffset!: number;
+  ThreeOffset!: number;
+  FourOffset!: number;
+  FiveOffset!: number;
+
+  public currentActive = 0;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+}
+
+  ngAfterViewInit() {
+    this.oneOffset = this.sectionOne.nativeElement.offsetTop;
+    this.TwoOffset = this.sectionTwo.nativeElement.offsetTop;
+    this.ThreeOffset = this.sectionThree.nativeElement.offsetTop;
+    this.FourOffset = this.sectionFour.nativeElement.offsetTop;
+    this.FiveOffset = this.sectionFive.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+
+  checkOffsetTop() {
+    if (window.pageYOffset >= this.oneOffset && window.pageYOffset < this.TwoOffset) {
+      this.currentActive = 1;
+    } else if (window.pageYOffset >= this.TwoOffset && window.pageYOffset < this.ThreeOffset) {
+      this.currentActive = 2;
+    } else if (window.pageYOffset >= this.ThreeOffset && window.pageYOffset < this.FourOffset) {
+      this.currentActive = 3;
+    } else if (window.pageYOffset >= this.FourOffset && window.pageYOffset < this.FiveOffset) {
+      this.currentActive = 4;
+    } else if (window.pageYOffset >= this.FiveOffset){
+      this.currentActive = 5;
+    } else {
+      this.currentActive = 0;
+    }
+  }
+}
